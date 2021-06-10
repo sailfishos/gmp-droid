1df5d9aa0cf7eb672f9154a25a143242688d2d37
Denis Grigorev <d.grigorev@omprussia.ru>
2021-06-10 15:50:26 +0300

    [gmp] Update copyright notices. JB#54538

--- generate-info.cpp
+++ generate-info.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (c) 2020 Open Mobile Platform LLC.
+** Copyright (c) 2020-2021 Open Mobile Platform LLC.
 **
 ** This Source Code Form is subject to the terms of the
 ** Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
